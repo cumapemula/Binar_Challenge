f23350eeedf13380df7c27d3578550356b31eedc
Taufiq Adjie Sanjaya <taufikaji350@gmail.com>
2023-03-16 23:41:37 +0700

    add: score, reset score, and backsound

--- public/script/app.js
+++ public/script/app.js
@@ -1,7 +1,4 @@
-// to fetch the current year in copyright
-// document.getElementById("year").innerHTML = new Date().getFullYear();
-
-// for main.html
+// inisialiasasi variabel
 const playerRock = document.getElementById("player-rock");
 const playerPaper = document.getElementById("player-paper");
 const playerScissors = document.getElementById("player-scissors");
@@ -11,9 +8,15 @@ const comScissors = document.getElementById("com-scissors");
 const playerChoice = document.querySelectorAll(".player-choice");
 const result = document.getElementById("result");
 const boxRandom = document.getElementById("box-random");
+const playerScore = document.getElementById("player-score");
+const comScore = document.getElementById("com-score");
+const resetScore = document.querySelector(".reset");
+const audio = document.getElementById("audio");
 
 let player;
 let computer;
+let playerTotal = 0;
+let comTotal = 0;
 
 playerChoice.forEach((button) =>
   button.addEventListener("click", () => {
@@ -22,17 +25,15 @@ playerChoice.forEach((button) =>
     setTimeout(() => {
       computerTurn();
       checkWinner();
-    }, 5000);
+      addAudio();
+      addScore();
+    }, 8000);
   })
 );
 
 function computerTurn() {
   const randomNumber = Math.floor(Math.random() * 3) + 1;
-
-  comRock.classList.remove("com-choice");
-  comPaper.classList.remove("com-choice");
-  comScissors.classList.remove("com-choice");
-
+  removeComClass();
   switch (randomNumber) {
     case 1:
       computer = comRock;
@@ -54,11 +55,8 @@ function computerTurn() {
 
 function computerSpin() {
   result.setAttribute("class", "result");
-
-  comRock.classList.remove("com-choice");
-  comPaper.classList.remove("com-choice");
-  comScissors.classList.remove("com-choice");
-
+  removeComClass();
+  audio.setAttribute("src", "./assets/audio/spin.mp3");
   boxRandom.classList.add("box-random");
 }
 
@@ -66,47 +64,56 @@ function checkWinner() {
   boxRandom.classList.remove("box-random");
 
   if (player.value == computer.value) {
-    return result.setAttribute(
-      "class",
-      "draw animate__animated animate__zoomIn"
-    );
+    return result.setAttribute("class", "draw");
   } else if (computer == comRock) {
     return player == playerPaper
-      ? result.setAttribute(
-          "class",
-          "player-win animate__animated animate__zoomIn"
-        )
-      : result.setAttribute(
-          "class",
-          "com-win animate__animated animate__zoomIn"
-        );
+      ? result.setAttribute("class", "player-win")
+      : result.setAttribute("class", "com-win");
   } else if (computer == comPaper) {
     return player == playerScissors
-      ? result.setAttribute(
-          "class",
-          "player-win animate__animated animate__zoomIn"
-        )
-      : result.setAttribute(
-          "class",
-          "com-win animate__animated animate__zoomIn"
-        );
+      ? result.setAttribute("class", "player-win")
+      : result.setAttribute("class", "com-win");
   } else if (computer == comScissors) {
     return player == playerRock
-      ? result.setAttribute(
-          "class",
-          "player-win animate__animated animate__zoomIn"
-        )
-      : result.setAttribute(
-          "class",
-          "com-win animate__animated animate__zoomIn"
-        );
+      ? result.setAttribute("class", "player-win")
+      : result.setAttribute("class", "com-win");
   }
 }
 
 document.querySelector(".refresh").addEventListener("click", () => {
   result.setAttribute("class", "result");
+  removeComClass();
+});
 
+function addScore() {
+  if (result.classList.value == "player-win") {
+    playerTotal++;
+    playerScore.innerText = playerTotal;
+  } else if (result.classList.value == "com-win") {
+    comTotal++;
+    comScore.innerText = comTotal;
+  }
+}
+
+resetScore.addEventListener("click", () => {
+  playerScore.innerText = 0;
+  comScore.innerText = 0;
+  removeComClass();
+  result.setAttribute("class", "result");
+});
+
+function removeComClass() {
   comRock.classList.remove("com-choice");
   comPaper.classList.remove("com-choice");
   comScissors.classList.remove("com-choice");
-});
+}
+
+function addAudio() {
+  if (result.classList.value == "player-win") {
+    audio.setAttribute("src", "./assets/audio/win.mp3");
+  } else if (result.classList.value == "com-win") {
+    audio.setAttribute("src", "./assets/audio/lose.mp3");
+  } else if (result.classList.value == "draw") {
+    audio.setAttribute("src", "./assets/audio/draw.mp3");
+  }
+}
