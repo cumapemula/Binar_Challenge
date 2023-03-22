f84483e4285e6f90394a77f5a16e7c65b60b0c1e
Taufiq Adjie Sanjaya <taufikaji350@gmail.com>
2023-03-22 22:30:57 +0700

    fix: styling

--- public/script/main.js
+++ public/script/main.js
@@ -39,6 +39,26 @@ class Player extends Games {
   constructor(choice) {
     super(choice);
   }
+
+  addBox() {
+    if (this.choice == "rock") {
+      document.getElementById("player-rock").classList.add("player-box");
+    } else if (this.choice == "paper") {
+      document.getElementById("player-paper").classList.add("player-box");
+    } else if (this.choice == "scissors") {
+      document.getElementById("player-scissors").classList.add("player-box");
+    }
+  }
+
+  removeBox() {
+    if (this.choice == "rock") {
+      document.getElementById("player-rock").classList.remove("player-box");
+    } else if (this.choice == "paper") {
+      document.getElementById("player-paper").classList.remove("player-box");
+    } else if (this.choice == "scissors") {
+      document.getElementById("player-scissors").classList.remove("player-box");
+    }
+  }
 }
 // Sub Class of Games
 class Computer extends Games {
@@ -90,20 +110,26 @@ function checkWinner() {
     audio.setAttribute("src", "./assets/audio/draw.mp3");
     return result.setAttribute("class", "draw");
   } else if (com.choice == "rock") {
-    return player.choice == "paper" ? games.setWin(true) : games.setWin(false);
+    return player.choice == "paper"
+      ? games.setWin(true)
+      : games.setWin(false);
   } else if (com.choice == "paper") {
     return player.choice == "scissors"
       ? games.setWin(true)
       : games.setWin(false);
   } else if (com.choice == "scissors") {
-    return player.choice == "rock" ? games.setWin(true) : games.setWin(false);
+    return player.choice == "rock"
+      ? games.setWin(true)
+      : games.setWin(false);
   }
 }
 
 playerSelected.forEach((button) => {
   button.addEventListener("click", () => {
+    player.removeBox();
     player.choice = button.value;
     console.log(`Player ${player.choice}`);
+    player.addBox();
     com.spin();
     const timeCheck = setTimeout(() => {
       com.turn();
@@ -112,20 +138,24 @@ playerSelected.forEach((button) => {
     // Reload / Refresh
     document.querySelector(".refresh").addEventListener("click", () => {
       clearTimeout(timeCheck);
+      player.removeBox();
+      com.removeBox();
+      boxSpin.classList.remove("box-spin");
+      result.setAttribute("class", "result");
+      audio.setAttribute("src", "");
+    });
+    // Reset Score
+    document.querySelector(".reset").addEventListener("click", () => {
+      clearTimeout(timeCheck);
+      games.pScore = 0;
+      games.cScore = 0;
+      playerScore.innerText = games.pScore;
+      comScore.innerText = games.cScore;
+      player.removeBox();
       com.removeBox();
       boxSpin.classList.remove("box-spin");
       result.setAttribute("class", "result");
       audio.setAttribute("src", "");
     });
   });
-});
-
-// Reset Score
-document.querySelector(".reset").addEventListener("click", () => {
-  games.pScore = 0;
-  games.cScore = 0;
-  playerScore.innerText = games.pScore;
-  comScore.innerText = games.cScore;
-  audio.setAttribute("src", "");
-  com.removeBox();
 });
